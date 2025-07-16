c3bc290e042dd06f6862570383d3241d1375b4b2
Agongaashi <157057758+Agongaashi@users.noreply.github.com>
2025-07-16 09:40:19 +0200

    commiti i pare
    krijimi i projektit

---
+++ Lab-1.Server/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Lab_1.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Lab_1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ lab-1.client/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div className="text-3xl font-bold text-blue-500 p-4">
+            Tailwind CSS po punon!
+        </div>
+    );
+}
+export default App;
